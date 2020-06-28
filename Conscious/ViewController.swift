fd46d68311e811d2efaecce75c1330d2c3fc96b3
Marquis Kurt <software@marquiskurt.net>
2020-06-28 08:09:50 -0400

    [CS400] Add Xcode project files, update README

---
+++ Conscious/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Conscious
+//
+//  Created by Marquis Kurt on 6/28/20.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! GameScene? {
+                
+                // Copy gameplay related content over to the scene
+                sceneNode.entities = scene.entities
+                sceneNode.graphs = scene.graphs
+                
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+                if let view = self.skView {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -6,16 +6,20 @@ An independent project on applying artificial intelligence with [GameplayKit](ht
 
 The primary objective of this project is to take the concepts from _CS440: Principles of Artificial Intelligence_ and build a better understanding of agents and apply these concepts by implementing and comparing agents in a video game.
 
+To view the milestones for this project, visit the [Milestones page in this repository's issues][milestones].
+
 ## Build instructions
 
 ### Requirements
 
 - Xcode 11 or higher
-- macOS 10.15 Catalina or higher
+- macOS 10.15 (Catalina) or higher
 
 Clone the Xcode project and then run the `<insertSchemeHere>` scheme to build the project in Xcode.
 
 ## Quick Links
 
 - [Project Proposal](PROPOSAL.md)
-- [Issue Tracker (YouTrack)](https://youtrack.marquiskurt.net/youtrack/issues/CS400)
+
+<!-- Links in page -->
+[milestones]: https://github.com/alicerunsonfedora/CS400/milestones?direction=asc&sort=due_date&state=open
