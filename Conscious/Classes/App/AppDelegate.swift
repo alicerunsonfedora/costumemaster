d14718c4bb12140d930c83b664b55aac67fc4ba7
Marquis Kurt <software@marquiskurt.net>
2020-08-27 15:33:50 -0400

    [Conscious] Add preferences window stuff

--- Conscious/Classes/App/AppDelegate.swift
+++ Conscious/Classes/App/AppDelegate.swift
@@ -10,8 +10,20 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    /// Show the preferences window for this application.
+    func instantiatePreferencesWindow() {
+        let prefs = NSStoryboard.init(name: NSStoryboard.Name("Preferences"), bundle: nil)
+        guard let controller: NSWindowController = prefs.instantiateController(
+                withIdentifier: "mainWindowController"
+        ) as? NSWindowController else {
+            return
+        }
+        controller.showWindow(self)
+    }
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
