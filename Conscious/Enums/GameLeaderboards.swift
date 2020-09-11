144e7ab2255b95b4037389a02f7269b4e77de295
Marquis Kurt <software@marquiskurt.net>
2020-09-11 18:15:28 -0400

    [Enums + Utilities + docs] Add GKLeaderboard utilities

---
+++ Conscious/Enums/GameLeaderboards.swift
@@ -0,0 +1,19 @@
+//
+//  GameLeaderboards.swift
+//  Costumemaster
+//
+//  Created by Marquis Kurt on 9/11/20.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import Foundation
+
+/// An enumeration for all possible leaderboards in the game.
+enum GameLeaderboard: String {
+    /// Daily Divergent Challenge: Complete the level **Divergent** as fast as you can.
+    /// - **ID**: costumemaster.ld.divergent_daily
+    case divergentDaily = "costumemaster.ld.divergent_daily"
+}
---
+++ Conscious/Utilities/Leaderboards.swift
@@ -0,0 +1,50 @@
+//
+//  Leaderboards.swift
+//  Costumemaster
+//
+//  Created by Marquis Kurt on 9/11/20.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import Foundation
+import GameKit
+
+extension GKLeaderboard {
+    /// Submit a score to the following leaderboard.
+    ///
+    /// This function applies to classic and recurring leaderboards.
+    ///
+    /// - Parameter leaderboard: The ID of the leaderboard.
+    /// - Parameter score: The score to submit to the leaderboard.
+    /// - Important: This function is only available on Macs running macOS 11.0.
+    @available(OSX 11.0, *) static func submit(to leaderboard: String, with score: Int) {
+        if !GKLocalPlayer.local.isAuthenticated { return }
+        GKLeaderboard.submitScore(
+            score,
+            context: 0,
+            player: GKLocalPlayer.local,
+            leaderboardIDs: [leaderboard]) { (error) in
+            guard error == nil else {
+                sendAlert(
+                    error.debugDescription,
+                    withTitle: "The score could not be sent to the leaderboard.",
+                    level: .critical) { _ in }
+                return
+            }
+        }
+    }
+
+    /// Submit a score to the following leaderboard.
+    ///
+    /// This function applies to classic and recurring leaderboards.
+    ///
+    /// - Parameter leaderboard: The game leaderboard ID to submit a score to.
+    /// - Parameter score: The score to submit to the leaderboard.
+    /// - Important: This function is only available on Macs running macOS 11.0.
+    @available(OSX 11.0, *) static func submit(to leaderboardID: GameLeaderboard, with score: Int) {
+        GKLeaderboard.submit(to: leaderboardID.rawValue, with: score)
+    }
+}
