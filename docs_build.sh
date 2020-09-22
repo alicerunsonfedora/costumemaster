e49e6a4c1f1da011324de0248895470d8ece039c
Marquis Kurt <software@marquiskurt.net>
2020-09-22 08:10:17 -0400

    [docs] Update documentation

--- docs_build.sh
+++ docs_build.sh
@@ -1,8 +1,17 @@
+
 echo "Starting Jazzy build..."
 bundle exec jazzy \
 --clean \
+--theme jony \
+--author "Marquis Kurt" \
+--author_url https://marquiskurt.net \
+--github_url https://github.com/alicerunsonfedora/CS400 \
+--min-acl internal \
+--title "The Costumemaster" \
+--exclude=/*/Classes/App/* \
+--documentation=Guides/*.md \
 --config .jazzy.yml \
---build-tool-arguments -workspace,Conscious.xcodeproj/project.xcworkspace,-scheme,"Conscious" \
+--build-tool-arguments -workspace,Conscious.xcodeproj/project.xcworkspace,-scheme,"Conscious"
 rm -r build
 echo costumemaster.marquiskurt.net >> docs/CNAME
 echo "Done."
