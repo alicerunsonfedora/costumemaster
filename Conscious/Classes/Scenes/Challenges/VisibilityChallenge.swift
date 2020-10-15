0c1f168a1bcf491e2581144299a4920a20c7d300
Marquis Kurt <software@marquiskurt.net>
2020-10-15 15:04:40 -0400

    :sparkles: [Classes + Scenes] Update Visibility with Now You See Me achievement

---
+++ Conscious/Classes/Scenes/Challenges/VisibilityChallenge.swift
@@ -0,0 +1,25 @@
+//
+//  VisibilityChallenge.swift
+//  Costumemaster
+//
+//  Created by Marquis Kurt on 10/15/20.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import Foundation
+import SpriteKit
+import GameKit
+
+/// The challenge scene associated with "Visibility".
+class VisibilityChallenge: ChallengeGameScene {
+    
+    /// Grant the "Now You See Me" achievement.
+    override func willCalculateChallengeResults() {
+        super.willCalculateChallengeResults()
+        GKAchievement.earn(with: .visibility)
+    }
+    
+}
