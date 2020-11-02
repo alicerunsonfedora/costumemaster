8980d5a05b425b9c598d6c0171c0e3bc5ea5998c
Marquis Kurt <software@marquiskurt.net>
2020-11-02 10:14:21 -0500

    :memo: [Utilities/Extensions/Scenes] Add comments to initConsole

--- Conscious/Utilities/Extensions/Scenes/AIGameScene_Extension.swift
+++ Conscious/Utilities/Extensions/Scenes/AIGameScene_Extension.swift
@@ -18,23 +18,31 @@ import SwiftUI
 @available(OSX 10.15, *)
 extension AIGameScene {
 
-    /// Initialize the console window.
+    /// Initialize the console window and begin streaming information to the console view model.
     func initConsole() {
+        // Get the name of the level and change it accordingly.
         var name = self.name ?? "AI Level"
         if name.hasSuffix("AI") {
             name = String(name.dropLast(name.suffix(2).count))
         }
+
+        // Create the hosting view for the console window from the SwiftUI view.
         let host = NSHostingView(rootView: AISimulatorConsole(console: self.console))
         let viewController = NSViewController()
         viewController.view = host
         viewController.title = "AI Simulator Console"
 
+        // Create the window that the console will live in and the appropriate interface changes such as
+        // dark mode.
         let window = NSWindow(contentViewController: viewController)
         window.styleMask.insert(.unifiedTitleAndToolbar)
+        window.appearance = NSAppearance(named: .darkAqua)
 
+        // Create the hosting view for the toolbar buttons.
         let toolbarButtons = NSHostingView(rootView: AISimulatorConsoleToolbar(console: self.console))
         toolbarButtons.frame.size = toolbarButtons.fittingSize
 
+        // Create the hosting view for the title and subtitle view of the window.
         let toolbarTitle = NSHostingView(
             rootView: VStack(alignment: .leading) {
                 Text(window.title)
@@ -45,6 +53,7 @@ extension AIGameScene {
         )
         toolbarTitle.frame.size = toolbarTitle.fittingSize
 
+        // Create the appropriate toolbar accessories for the title and the toolbar buttons.
         let accessory = NSTitlebarAccessoryViewController()
         accessory.view = toolbarButtons
         accessory.layoutAttribute = .trailing
@@ -53,11 +62,13 @@ extension AIGameScene {
         titleAccessory.view = toolbarTitle
         titleAccessory.layoutAttribute = .leading
 
+        // Create a toolbar on the window and add the accessories.
         window.toolbar = NSToolbar()
         window.addTitlebarAccessoryViewController(titleAccessory)
         window.addTitlebarAccessoryViewController(accessory)
         window.titleVisibility = .hidden
 
+        // Show the window and try to make it the key window.
         let windowController = NSWindowController(window: window)
         windowController.showWindow(self)
         windowController.window?.makeKey()
