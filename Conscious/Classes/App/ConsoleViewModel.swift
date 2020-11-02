2801a4f516913dfa974f46f03717f645bc85d547
Marquis Kurt <software@marquiskurt.net>
2020-11-02 13:11:31 -0500

    :memo: [Classes/App] Add documentation to ConsoleViewModel

--- Conscious/Classes/App/ConsoleViewModel.swift
+++ Conscious/Classes/App/ConsoleViewModel.swift
@@ -16,28 +16,56 @@ import Combine
 #endif
 
 @available(OSX 10.15, *)
+/// A class for handling console output.
 public class ConsoleViewModel: ObservableObject {
+    
+    /// The list of console messages that have been logged.
     @Published public var messages: [Message] = []
+    
+    /// The filter for the types of console messages that can be displayed.
     @Published public var filter: [MessageType] = [.info, .debug, .error, .warning, .unknown]
+    
+    /// Whether to enable "now mode".
     @Published public var nowMode: Bool = false
 
+    /// An enumeration that represents the type of messages that can be logged.
     public enum MessageType: String {
+        
+        /// An informational message.
         case info = "INFO"
+        
+        /// A warning.
         case warning = "WARN"
+        
+        /// An error message.
         case error = "ERR"
+        
+        /// An unknown type or standard log.
         case unknown = "LOG"
+        
+        /// A debugging message.
         case debug = "DEBUG"
     }
 
+    /// A data structure that represents a console message.
     public struct Message: Identifiable {
+        
+        /// The contents of the message.
         public let contents: String
+        
+        /// The type of message.
         public let type: MessageType
+        
+        /// The time the message was created or logged.
         public let timestamp: String
+        
+        /// A(n) unique identifier for this message.
         public let id = UUID()
         //swiftlint:disable:previous identifier_name
     }
 
     // swiftlint:disable:next large_tuple
+    /// Returns a tuple containing the hour, minute, and second logged.
     private func time() -> (Int, Int, Int) {
         let date = Date()
         let calendar = NSCalendar.current
@@ -53,31 +81,43 @@ public class ConsoleViewModel: ObservableObject {
         if !silent { print("[\(type.rawValue)]\t\(timestamp)\t" + data) }
         messages.append(Message(contents: data, type: type, timestamp: timestamp))
     }
-
-    public func lastMessage() -> Message? {
-        messages.filter { message in filter.contains(message.type) }.last
-    }
-
+    
+    /// Log a message to the console.
+    /// - Parameter message: The contents of the message entry.
+    /// - Parameter silent: Whether to skip printing the message to the terminal.
     public func log(_ message: String, silent: Bool = false) {
         self.sendMessage(with: message, type: .unknown, silent: silent)
     }
 
+    /// Log an informational message to the console.
+    /// - Parameter message: The contents of the message entry.
+    /// - Parameter silent: Whether to skip printing the message to the terminal.
     public func info(_ message: String, silent: Bool = false) {
         self.sendMessage(with: message, type: .info, silent: silent)
     }
 
+    /// Log a warning to the console.
+    /// - Parameter message: The contents of the message entry.
+    /// - Parameter silent: Whether to skip printing the message to the terminal.
     public func warn(_ message: String, silent: Bool = false) {
         self.sendMessage(with: message, type: .warning, silent: silent)
     }
 
+    /// Log an error message to the console.
+    /// - Parameter message: The contents of the message entry.
+    /// - Parameter silent: Whether to skip printing the message to the terminal.
     public func error(_ message: String, silent: Bool = false) {
         self.sendMessage(with: message, type: .error, silent: silent)
     }
 
+    /// Log a debugging message to the console.
+    /// - Parameter message: The contents of the message entry.
+    /// - Parameter silent: Whether to skip printing the message to the terminal.
     public func debug(_ message: String, silent: Bool = true) {
         self.sendMessage(with: message, type: .debug, silent: silent)
     }
 
+    /// Clears the console.
     public func clear() {
         self.messages = []
         self.info("Console cleared.", silent: true)
